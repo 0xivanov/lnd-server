b8aa8cb48f8017f0e43b133f470439c7cda9b1b9
Ivan Ivanov <ivanivanov.ii726@gmail.com>
2023-09-30 13:56:19 +0300

    fix createHmac

--- api/index.js
+++ api/index.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const crypto = require('crypto');
+const { createHmac } = require('node:crypto');
 const app = express();
 const port = 3001; // Choose a port for your API
 
 // Middleware to parse JSON requests
 app.use(express.json());
 
-app.get('/kur', (req, res) => {
-  res.end(`Hello! Go to item:`);
-});
 app.get('/home', (req, res) => {
   res.end(`Hello! Go to item:`);
 });
@@ -17,7 +15,7 @@ app.post('/test', (req, res) => {
   console.log("HI")
   const charge = req.body;
   const received = charge.hashed_order;
-  const calculated = crypto.createhmac('sha256', 'e92064ab-0799-467c-8876-25bb1a393422').update(charge.id).digest('hex');
+  const calculated = createHmac('sha256', 'e92064ab-0799-467c-8876-25bb1a393422').update(charge.id).digest('hex');
   if (received === calculated) {
     console.log("signiture is valid")
     res.status(200).json({ message: 'payment received successfully' });
