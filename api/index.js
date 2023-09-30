225f0090e14c537acfdbddbc36d6acab31d0202a
Ivan Ivanov <ivanivanov.ii726@gmail.com>
2023-09-30 13:30:58 +0300

    add log

--- api/index.js
+++ api/index.js
@@ -11,15 +11,19 @@ app.get('/home', (req, res) => {
 });
 
 app.post('/payment', (req, res) => {
-  const charge = req.body;
-  const received = charge.hashed_order;
-  const calculated = crypto.createHmac('sha256', 'e92064ab-0799-467c-8876-25bb1a393422').update(charge.id).digest('hex');
-  if (received === calculated) {
-    console.log("signiture is valid")
-    res.status(200).json({ message: 'Payment received successfully' });
-  }
-  else {
-    console.log("signiture is invalid")
+  try {
+    const charge = req.body;
+    const received = charge.hashed_order;
+    const calculated = crypto.createHmac('sha256', 'e92064ab-0799-467c-8876-25bb1a393422').update(charge.id).digest('hex');
+    if (received === calculated) {
+      console.log("signiture is valid")
+      res.status(200).json({ message: 'Payment received successfully' });
+    }
+    else {
+      console.log("signiture is invalid")
+    }
+  } catch (error) {
+    console.log(error)
   }
 
 });
